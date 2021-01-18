dbd323a50b48988d3a43c11597cd28e30923d55f
Student <student@Students-MacBook-Pro.local>
2021-01-19 09:58:03 +1100

    added play pause function

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+
+    const audioRef = useRef(null);
+
+    const playSongHandler = () => {
+        if(isPlaying){
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else{
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    }
+
     return (
         <div className="player">
-            <h1>Player</h1>
+            <div className="time-control">
+                <p>Start</p>
+                <input type="range" />
+                <p>End</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+            </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Song = () => {
+const Song = ({ currentSong }) => {
     return (
-        <div className="Song">
-            <h1>Song</h1>
+        <div className="song-container">
+            <img src={currentSong.cover}></img>
+            <h2>{currentSong.name}</h2>
+            <h3>{currentSong.artist}</h3>
         </div>
     )
 }
